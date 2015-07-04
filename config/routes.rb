1623091821f816eeefa6837b34a0f6e0d2e7044e
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-04 11:23:56 -0700

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :users
+
+  get '/login'     => 'sessions#new'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
