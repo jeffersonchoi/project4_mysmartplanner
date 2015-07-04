9aec11d3e443e1cdaf904476e9d170f84484e4c5
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-04 12:16:54 -0700

    add guest to user migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704080032) do
+ActiveRecord::Schema.define(version: 20150704191636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150704080032) do
     t.string   "gender"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "guest"
   end
 
   add_foreign_key "events", "itineraries"
