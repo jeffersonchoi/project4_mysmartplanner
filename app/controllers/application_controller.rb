26de2d09a0b1be07e4df9aa1ab67ffb3ef061148
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-04 11:26:18 -0700

    working user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
    def create
      @user = User.new(user_params)
      if @user.save
+       session[:user_id] = @user.id.to_s
        redirect_to users_path
      else
        render :new
