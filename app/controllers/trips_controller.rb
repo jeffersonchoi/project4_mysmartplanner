213c73a49b95ff4f698e658435ca5ff7d86a2188
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-04 14:09:01 -0700

    trips controller index show new create

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,26 @@
 class TripsController < ApplicationController
-  
+
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+  end
+
+private
+
+   def trip_params
+     params.require(:trip).permit(:destination, :start_date, :end_date, :budget, :hours_per_day, :interest)
+   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :users
+  resources :trips
 
   get '/login'     => 'sessions#new'
   post '/login'    => 'sessions#create'
