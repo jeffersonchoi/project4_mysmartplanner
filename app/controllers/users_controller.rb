ec96959871a249e421a143284a8a99c604be194e
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-04 11:41:09 -0700

    add user show, edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
      @users = User.all
    end
 
+   def show
+     @user = User.find(params[:id])
+   end
+
    def new
      @user = User.new
    end
@@ -18,7 +22,21 @@ class UsersController < ApplicationController
      end
    end
 
-   private
+   def edit
+    @user = User.find(current_user)
+   end
+
+   def update
+    @user = User.find(current_user)
+
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+   end
+
+private
 
    def user_params
      params.require(:user).permit(:first_name, :last_name, :gender, :email, :password, :password_confirmation, :date_of_birth)
