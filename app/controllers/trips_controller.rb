56fb7bb1e27fcec4c7cee099cef6d02cd2ebdb11
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-06 18:27:01 -0700

    trip_controller working itinerary create

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -16,8 +16,39 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     @trip.user_id = current_user.id
+
     if @trip.save
 
+      # if @trip.hours_per_day % 3 == 0
+        no_of_itinerary = (@trip.hours_per_day / 3).floor
+        for i in 1..no_of_itinerary
+          @add_itinerary = Itinerary.new(trip_id: @trip.id)
+          @add_itinerary.save
+
+          if @add_itinerary.save
+
+            @add_event = Event.new(itinerary_id: @add_itinerary.id, node_id: @trip.interest )
+
+          end
+
+        end
+
+      # elsif @trip.hours_per_day % 3 == 1
+      #   no_of_itinerary = @trip.hours_per_day / 3 + 1
+      #   for i in 1..no_of_itinerary
+      #     @add_itinerary = Itinerary.new(trip_id: @trip.id)
+      #     @add_itinerary.save
+      #   end
+      # else
+      #   no_of_itinerary = @trip.hours_per_day / 3 + 2
+      #   for i in 1..no_of_itinerary
+      #     @add_itinerary = Itinerary.new(trip_id: @trip.id)
+      #     @add_itinerary.save
+      #   end
+      # end
+
+
+      # puts @trip.inspect
 
       redirect_to trips_path
     else
