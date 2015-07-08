46a827ed7ce49cbd8feb0e34fae963d8e904d0c0
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-08 16:18:01 -0700

    working start_time

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -43,6 +43,7 @@ class TripsController < ApplicationController
       end
 
 
+          start_time = Event.calculate_start_time(@trip.hours_per_day)
             # if @trip.hours_per_day % 3 == 0
           no_of_itinerary = (@trip.hours_per_day / 3).floor
           for i in 1..no_of_itinerary
@@ -51,9 +52,15 @@ class TripsController < ApplicationController
 
             if @add_itinerary.save
 
-              @add_event = Event.new(itinerary_id: @add_itinerary.id, node_id: events.sample)
 
+              end_time = start_time + 3.hours
+
+              @add_event = Event.new(itinerary_id: @add_itinerary.id, node_id: events.sample, start_time: start_time, end_time: end_time)
+              events.delete_if{|b| b == @add_event.node_id}
               @add_event.save
+
+              start_time = end_time
+
               # that_event_id = @add_event.node_id + 10
               # @add_event = Event.new(itinerary_id: @add_itinerary.id, node_id: that_event_id)
             end
