f31f6b3dcf9219264e49328d3dd1bb1974273002
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-13 09:00:23 -0700

    add validation for trip.rb

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -22,12 +22,12 @@ RSpec.describe Trip, type: :model do
       expect(trip).to be_invalid
     end
     it "is invalid if start_date is before current date" do
-      # trip = Trip.new(destination: "Los Angeles", start_date: "2015-9-12", end_date: "2015-9-12", budget: 123, hours_per_day: 6, interest: nil)
-      # expect(trip).to be_invalid
+      trip = Trip.new(destination: "Los Angeles", start_date: "2015-7-12", end_date: "2015-9-12", budget: 123, hours_per_day: 6, interest: "theme_park")
+      expect(trip).to be_invalid
     end
     it "is invalid if end_date is before startp' date" do
-      # trip = Trip.new(destination: "Los Angeles", start_date: "2015-9-12", end_date: "2015-9-12", budget: 123, hours_per_day: 6, interest: nil)
-      # expect(trip).to be_invalid
+      trip = Trip.new(destination: "Los Angeles", start_date: "2015-9-12", end_date: "2015-9-10", budget: 123, hours_per_day: 6, interest: "theme_park")
+      expect(trip).to be_invalid
     end
 
 end
