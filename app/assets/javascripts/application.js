3e67b1d1e33988bb929f3093c411c29e5a71d70c
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-13 18:19:31 -0700

    google map adds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,34 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'page:load', initialize);
+
+function initialize() {
+  var mapProperties = {
+  center:new google.maps.LatLng(34.0129821,-118.495196),
+  zoom: 17,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  var map=new google.maps.Map(document.getElementById("googleMap"), mapProperties);
+
+  var marker=new google.maps.Marker({
+  position:new google.maps.LatLng(34.0129821,-118.495196),
+  animation:google.maps.Animation.BOUNCE //animates the marker
+
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+  content:"Hello World!"
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+  infowindow.open(map,marker);
+  });
+
+marker.setMap(map);
+
+
+};
