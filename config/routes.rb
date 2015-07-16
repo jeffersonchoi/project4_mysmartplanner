9e1c0451125db7a15c099dcbc815e9a891e9c342
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-16 12:03:54 -0700

    Revert "Bookerpro"

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   get '/login'     => 'sessions#new'
   post '/login'    => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  get '/travizi' => 'staticpages#travizi'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
