5c7b87a4575cfeb8186a048d45c2ebcd4e0da43b
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-16 17:54:24 -0700

    try to make testing work

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,19 +3,17 @@ require 'rails_helper'
 feature "a user can create an account" do
   scenario "valid registration" do
    visit new_user_path
-  #  fill_in "First name", with: "Test"
-  #  fill_in "Last name", with: "test"
-  #  choose "user[gender]", option: 'male'
-  #  fill_in "Email", with: "testtesttest@test.com"
-  #  select '1991', from: "user[dob(1i)]"
-  #  select 'September', from: "user[dob(2i)]"
-  #  select '12', from: "user[dob(3i)]"
+   fill_in "First name", with: "Test"
+   fill_in "Last name", with: "test"
+   choose "user[gender]", option: 'male'
+   fill_in "Email", with: "testtesttesttest@test.com"
+   fill_in "user[dob]", with: "12/09/1991"
    fill_in "user[password]", with: "12345678"
    fill_in "user[password_confirmation]", with: "12345678"
    #
    #
    click_on("Register")
 
-   expect(page).to have_content("error")
+   expect(page).to have_content("Attractions")
   end
 end
