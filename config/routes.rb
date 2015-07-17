d570bf1b07bbf7d910782569797611ae68bfedd6
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-16 21:38:29 -0700

    add jefferson profile

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get '/madliberator' => 'staticpages#madliberator'
   get '/appliedmeaning' => 'staticpages#appliedmeaning'
   get '/qwickbit' => 'staticpages#qwickbit'
+  get '/developer' => 'staticpages#jefferson'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
